c60d31c0b609255a6a76ebcc7095b1383fc19dd5
Sota Watanabe <w.souta.s.life@gmail.com>
2017-11-24 20:49:15 -0800

    2017/11/24

---
+++ 徹底マスターJSの教科書/AlermPromptConfirm.js
@@ -0,0 +1,14 @@
+//ダイアログの表示
+//警告ダイアログ
+alert("渡辺創太");
+
+//入力ダイアログ
+prompt("名前を入力してください");
+var name = prompt("名前を入力してください");
+//数字を返したいことはparseInt,paeseFloat
+var age = parseInt(prompt("年齢を入力してください"));
+var height = parseFloat(prompt("身長を入力してください"));
+
+//確認用ダイアログ
+var ret = confirm("リンクを表示してよろしいでしょうか？");
+//true or falseの戻り値になる
